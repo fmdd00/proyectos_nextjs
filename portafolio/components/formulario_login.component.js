e5ff85e7646fbc5e478c88ebb85fe3df1620a962
fmdd00 <francisco63554@hotmail.com>
2022-02-08 15:13:08 -0500

    actualizado nextjs con ajax

--- portafolio/components/formulario_login.component.js
+++ portafolio/components/formulario_login.component.js
@@ -1,4 +1,25 @@
-const FormularioLogin = () => {
+import { useState } from "react"
+
+const FormularioLogin = (props) => {
+
+    // Variables internas (variables de estado) del componente
+    const [txtUsername, setTxtUsername] = useState("")
+    const [txtPassword, setTxtPassword] = useState("")
+
+    const txtUsernameOnChange = (event) => {
+        const txtUsernameIngresado = event.target.value
+        setTxtUsername(txtUsernameIngresado)
+    }
+
+    const txtPasswordOnChange = (event) => {
+        setTxtPassword(event.target.value)
+    }
+
+    const butLoginOnClick = () => {
+        // Se va a ejecutar la funcion que se paso por el props (onLogin)
+        props.onLogin(txtUsername, txtPassword)
+    }
+
     return <aside className="col-md-4">
         <div className="card">
             <div className="card-body">
@@ -6,15 +27,26 @@ const FormularioLogin = () => {
                 <form>
                     <div>
                         <label htmlFor="txt_username" className="form-label">Username</label>
-                        <input id="txt_username" type="text" className="form-control" />
+                        <input id="txt_username" type="text" className="form-control" 
+                               defaultValue={ txtUsername } onChange={ txtUsernameOnChange }/>
                     </div>
                     <div className="mb-2">
                         <label htmlFor="txt_password" className="form-label">Password</label>
-                        <input id="txt_password" type="password" className="form-control" />
+                        <input id="txt_password" type="password" className="form-control" 
+                               defaultValue={ txtPassword } onChange={ txtPasswordOnChange }/>
                     </div>
-                    <button id="butLogin" className="btn btn-primary" type="button">Login</button>
+                    <button id="butLogin" className="btn btn-primary" type="button"
+                            onClick={ butLoginOnClick }>Login</button>
                     <a href="#">Registro</a>
                 </form>
+                
+                {
+                    (() => {
+                        if (props.error) {
+                            return <div className="alert alert-danger mt-2">Error en login</div>
+                        }
+                    })()
+                }
             </div>
         </div>
     </aside>
--- portafolio/pages/index.js
+++ portafolio/pages/index.js
@@ -3,14 +3,44 @@ import Banner from "../components/banner.component";
 import ListaProyectos from "../components/lista_proyectos.component";
 import FormularioLogin from "../components/formulario_login.component";
 import Footer from "../components/footer.component";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    const listadoProyectos = [
-        {nombre : "Proyecto 1", usuario: "billy", puntaje : 4.9},
-        {nombre : "Proyecto 2", usuario: "hernan", puntaje : 4.7},
-        {nombre : "Proyecto 3", usuario: "jorge", puntaje : 4.4},
-        {nombre : "Proyecto 4", usuario: "kory", puntaje : 4.3}
-    ]
+    // http://demo9667197.mockable.io/proyectos
+
+    // const listadoProyectos = [
+    //     {nombre : "Proyecto 1", usuario: "billy", puntaje : 4.9},
+    //     {nombre : "Proyecto 2", usuario: "hernan", puntaje : 4.7},
+    //     {nombre : "Proyecto 3", usuario: "jorge", puntaje : 4.4},
+    //     {nombre : "Proyecto 4", usuario: "kory", puntaje : 4.3}
+    // ]
+
+    const [errorLogin, setErrorLogin] = useState(false)
+    const [listadoProyectos, setListadoProyectos] = useState([])
+    const [listaImagenes, setListaImagenes] = useState([])
+
+    useEffect(async () => {
+        // Llamada AJAX para cargar listado de proyectos
+        let response = await fetch("http://demo9667197.mockable.io/proyectos")
+        const data = await response.json()
+        setListadoProyectos(data.proyectos)
+
+        // Llamada AJAX para cargar lista de urls de imagenes
+        response = await fetch("http://demo9667197.mockable.io/")
+        const dataImagenes = await response.json()
+        setListaImagenes(dataImagenes.images)
+
+    }, [])
+
+    const loginHandler = (username, password) => {
+        if (username == "billy" && password == "123") {
+            location.href = "/main"
+        }else {
+            console.log("Error en login")
+            setErrorLogin(true)
+        }
+    }
+
     return <div>
         <div>
             <header>
@@ -19,10 +49,10 @@ export default function Home() {
             <MenuNavegacion />
         </div>
         <div className="mt-4">
-            <Banner />
+            <Banner images={ listaImagenes }/>
             <div className="row mt-4">
                 <ListaProyectos proyectos={ listadoProyectos } />
-                <FormularioLogin />
+                <FormularioLogin onLogin={ loginHandler } error={ errorLogin }/>
             </div>
         </div>
         <Footer />
